771ff16c61587ecdade74eba22b16d8c986f5d9d
Malte Wilhelm <malte.wilhelm93+git@gmail.com>
2020-09-11 16:32:56 +0200

    Add flask app

--- index.py
+++ index.py
@@ -1,18 +1,14 @@
-
-
 from flask import Flask, render_template
-
+from scraper import scrap
 
 app = Flask(__name__)
 
-
 @app.route("/") ##www.domain.com
 def index():
-
-
-    return render_template("index.html")
+    date,post_text,raw_text,post_url = scrap()
+    zipped_list = zip(*post_text)
+    return render_template("index.html",date=date,zipped_list=zipped_list,raw_text=raw_text,post_url=post_url)
 
 
 if __name__ == "__main__":
-
     app.run(debug=False)
