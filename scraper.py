eb132e76d0bcb06eaf0a81c8a66cc03970e8e9be
Malte Wilhelm <malte.wilhelm93+git@gmail.com>
2020-09-11 16:33:19 +0200

    Update scraper to allow import (module)

--- scraper.py
+++ scraper.py
@@ -11,52 +11,69 @@ pagename = 'eismanufakturzeitgeist'
 # by get_posts
 post = next(get_posts(pagename, pages=1))
 
-# handle date of post
-# assumption: ice cream flavour are posted the same day
-date = post['time']  # grab date
-# TODO: if the post is from today skip repeated checks
-if date.strftime('%Y-%m-%d') == datetime.today().strftime('%Y-%m-%d'):
-    print("Post is from today")
-date = date.strftime('%A, %d.%m.%Y')  # format datetime object
+def scrap():
+    # handle date of post
+    # assumption: ice cream flavour are posted the same day
+    date = post['time']  # grab date
+    # TODO: if the post is from today skip repeated checks
+    if date.strftime('%Y-%m-%d') == datetime.today().strftime('%Y-%m-%d'):
+        print("Post is from today")
+    date = date.strftime('%A, %d.%m.%Y')  # format datetime object
 
-# get post text (String)
-post_text = post['text']
+    # get post text (String)
+    post_text = post['text']
+    post_url = post['post_url']
+    raw_text = post_text
 
-# assumption: vanilla and chocolate are always on offer
-# so if the post does not contain vanilla and chocolate it is not about ice
-# cream
+    # assumption: vanilla and chocolate are always on offer
+    # so if the post does not contain vanilla and chocolate it is not about ice
+    # cream flavours
 
-if "Vanille" and "Schokolade" not in post_text:
-    print("No ice cream update.")
-else:
-    # remove everything before Lindenhof and after up until the :
-    # TODO: This breaks if Limburgerhof is posted first
-    regex = re.compile(r'.*?Lindenhof.*?:', re.DOTALL)
-    post_text = re.sub(regex, 'Lindenhof', post_text)
+    if "Vanille" and "Schokolade" not in post_text:
+        print("No ice cream update.")
+    else:
+        # remove everything before Lindenhof and after up until the :
+        # TODO: This breaks if Limburgerhof is posted first
+        regex = re.compile(r'.*?Lindenhof.*?:', re.DOTALL)
+        post_text = re.sub(regex, 'Lindenhof', post_text)
 
-    # split on linebreaks
-    post_text = re.split('[ ]*\n+[ ]*', post_text.strip())
+        # split on linebreaks
+        post_text = re.split('[ ]*\n+[ ]*', post_text.strip())
 
-    # remove last sentence
-    if "schmecken" in post_text[-1]:
-        del post_text[-1]
+        # remove last sentence
+        if "schmecken" in post_text[-1]:
+            del post_text[-1]
 
-    regex = re.compile(r'.*?Limburgerhof.*?:', re.DOTALL)
+        regex = re.compile(r'.*?Limburgerhof.*?:', re.DOTALL)
 
-    for i, x in enumerate(post_text):
-        if 'Limburgerhof' in x:
-            # remove everything before Limburgerhof and after up until the :
-            post_text[i] = re.sub(regex, 'Limburgerhof', x)
+        for i, x in enumerate(post_text):
+            if 'Limburgerhof' in x:
+                # remove everything before Limburgerhof and after up until the :
+                post_text[i] = re.sub(regex, 'Limburgerhof', x)
 
-    # split on , & 'und'
-    for i, x in enumerate(post_text):
-        post_text[i] = re.split(' und |[,]', x)
+
+
+        # split on ',' & 'und'
+        for i, x in enumerate(post_text):
+            post_text[i] = re.split(' und |[,]', x)
+
+            # change only ice cream flavours located at 1 and 3 at list
+            if i % 2 != 0:
+                for j, y in enumerate(post_text[i]):
+                    post_text[i][j] = y.lstrip() # string are imutable
+
+
 
     # export csv
-    with open('csv/' + date + '.csv', 'w', newline='') as file:
+    toCSV(date,post_text)
+    return date,post_text,raw_text,post_url
+
+
+def toCSV(date,post_text):
+    with open('../csv/' + date + '.csv', 'w', newline='') as file:
         writer = csv.writer(file, delimiter=',',)
         # write header and remove entries from list
-        writer.writerow([*post_text.pop(0), *post_text.pop(1), ])
+        writer.writerow([*post_text.pop(0), *post_text.pop(1)])
 
         zipped_list = zip(*post_text)
         for x in zipped_list:
